ae586b5d3a1b15b092fc379518bb67bde952abca
3barney <brnbsjm@gmail.com>
2022-07-14 20:24:24 +0300

    Endpoints and temp storage of data

--- server.go
+++ server.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"github.com/go-chi/chi"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
+var StoragePath = "/tmp"
 var data = map[string]string{}
 
 func main() {
@@ -129,4 +132,92 @@ func JSON(writer http.ResponseWriter, data interface{}) {
 		return
 	}
 	writer.Write(body)
+}
+
+func dataPath() string {
+	return filepath.Join(StoragePath, "data.json")
+}
+
+func loadData(ctx context.Context) (map[string]string, error) {
+	empty := map[string]string{}
+	emptyData, err := encode(map[string]string{})
+	if err != nil {
+		return empty, err
+	}
+
+	// First check if the folder exists and create it if it is missing.
+	if _, err := os.Stat(StoragePath); os.IsNotExist(err) {
+		err := os.MkdirAll(StoragePath, 0755)
+		if err != nil {
+			return empty, err
+		}
+	}
+
+	// Then check if the file exists and create it if it is missing.
+	if _, err := os.Stat(dataPath()); os.IsNotExist(err) {
+		err := os.WriteFile(dataPath(), emptyData, 0644)
+		if err != nil {
+			return empty, err
+		}
+	}
+
+	content, err := os.ReadFile(dataPath())
+	if err != nil {
+		return empty, err
+	}
+
+	return decode(content)
+}
+
+func saveData(ctx context.Context, data map[string]string) error {
+
+	if _, err := os.Stat(StoragePath); os.IsNotExist(err) {
+		err = os.MkdirAll(StoragePath, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	encodedData, err := encode(data)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(dataPath(), encodedData, 0644)
+}
+
+func encode(data map[string]string) ([]byte, error) {
+	encodedData := map[string]string{}
+	for k, v := range data {
+		ek := base64.URLEncoding.EncodeToString([]byte(k))
+		ev := base64.URLEncoding.EncodeToString([]byte(v))
+		encodedData[ek] = ev
+	}
+
+	return json.Marshal(encodedData)
+}
+
+func decode(data []byte) (map[string]string, error) {
+	var jsonData map[string]string
+
+	if err := json.Unmarshal(data, &jsonData); err != nil {
+		return nil, err
+	}
+
+	returnData := map[string]string{}
+	for k, v := range jsonData {
+		dk, err := base64.URLEncoding.DecodeString(k)
+		if err != nil {
+			return nil, err
+		}
+
+		dv, err := base64.URLEncoding.DecodeString(v)
+		if err != nil {
+			return nil, err
+		}
+
+		returnData[string(dk)] = string(dv)
+	}
+
+	return returnData, nil
 }
