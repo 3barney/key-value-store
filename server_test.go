bcb11035b0b23090fca7bc7c98ce66c58a2e468c
3barney <brnbsjm@gmail.com>
2022-07-20 22:31:00 +0300

    Add simple Benchamark and mock intergration test

--- server_test.go
+++ server_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -47,3 +51,98 @@ func TestJSON(t *testing.T) {
 		}
 	}
 }
+
+// TODO: implement edge cases using table testing
+func TestGet(t *testing.T) {
+	makeStorage(t)
+	defer cleanupStorage(t)
+
+	key := "key"
+	value := "value"
+	encodedKey := base64.URLEncoding.EncodeToString([]byte(key))
+	encodedValue := base64.URLEncoding.EncodeToString([]byte(value))
+	fileContents, _ := json.Marshal(map[string]string{encodedKey: encodedValue})
+	err := os.WriteFile(StoragePath+"/data.json", fileContents, 0644)
+	if err != nil {
+		return
+	}
+
+	got, err := Get(context.Background(), key)
+	if err != nil {
+		t.Errorf("Received unexpected error: %s", err)
+	}
+	if got != value {
+		t.Errorf("Got %s, expected %s", got, value)
+	}
+}
+
+// Run go test -bench .
+func BenchmarkGet(b *testing.B) {
+	makeStorage(b)
+	defer cleanupStorage(b)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Get(context.Background(), "key1")
+	}
+}
+
+// Simple Intergration Test
+// GET "key" -> SET -> GET -> DELETE -> GET
+func TestGetSetDelete(t *testing.T) {
+	// t.Parallel()
+	makeStorage(t)
+	defer cleanupStorage(t)
+	ctx := context.Background()
+
+	key := "key"
+	value := "value"
+
+	if out, err := Get(ctx, key); err != nil || out != "" {
+		t.Fatalf("First Get returned unexpected result, out: %q, error: %s", out, err)
+	}
+
+	if err := Set(ctx, key, value); err != nil {
+		t.Fatalf("Set returned unexpected error: %s", err)
+	}
+
+	if out, err := Get(ctx, key); err != nil || out != value {
+		t.Fatalf("Second Get returned unexpected result, out: %q, error: %s", out, err)
+	}
+
+	if err := Delete(ctx, key); err != nil {
+		t.Fatalf("Delete returned unexpected error: %s", err)
+	}
+
+	if out, err := Get(ctx, key); err != nil || out != "" {
+		t.Fatalf("Third Get returned unexpected result, out: %q, error: %s", out, err)
+	}
+}
+
+// Accect both Testing and Benchmark tests
+func makeStorage(tb testing.TB) {
+	err := os.Mkdir("testdata", 0755)
+	if err != nil && !os.IsExist(err) {
+		tb.Fatalf("Couldn't create directory testdata: %s", err)
+	}
+
+	StoragePath = "testdata"
+}
+
+func cleanupStorage(tb testing.TB) {
+	if err := os.RemoveAll(StoragePath); err != nil {
+		tb.Errorf("Failed to delete storage path: %s", StoragePath)
+	}
+
+	StoragePath = "/tmp/kv"
+}
+
+// OLD TEsting only implementation
+//func makeStorage(t *testing.T) {
+//	err := os.Mkdir("testdata", 0755)
+//	if err != nil && !os.IsExist(err) {
+//		tb.Fatalf("Couldn't create directory testdata: %s", err)
+//	}
+//
+//	StoragePath = "testdata"
+//}
